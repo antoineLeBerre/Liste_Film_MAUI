ee67bdf77982353d34e9c89f6d3502398ac5c612
antoineleberre <ant.le.berre@gmail.com>
2023-09-12 22:52:12 +0200

    First Commit

---
+++ ListeFilm/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ListeFilm;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ ListeFilm/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ListeFilm;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
